30da5a8dd6fca7782bc13a2a30c039e6530dddc7
chappjc <jonathan.chappelow@gmail.com>
2015-04-30 10:35:30 -0700

    Remove CUDA clutter.

--- include/pqheap.cpp
+++ include/pqheap.cpp
@@ -4,29 +4,26 @@
 #include <cassert>
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 PQheap<T,C,S>::PQheap()
 : heap(NULL), N(0), cap(0)
 {
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 PQheap<T,C,S>::PQheap(unsigned int maxSize)
-: heap(NULL), N(0), cap(maxSize)
+: heap(NULL)
 {
     init(cap);
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 int PQheap<T,C,S>::init(unsigned int maxSize)
 {
-    if (maxSize == 0)
-        return 0;
-
     N = 0;
     cap = maxSize;
+	
+	if (maxSize == 0)
+        return 0;
 
     if (heap)
         delete[] heap;
@@ -42,7 +39,6 @@ int PQheap<T,C,S>::init(unsigned int maxSize)
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 PQheap<T,C,S>::PQheap(T* a, unsigned int capacity)
 {
     bind(a, capacity);
@@ -50,7 +46,6 @@ PQheap<T,C,S>::PQheap(T* a, unsigned int capacity)
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 int PQheap<T,C,S>::bind(T* a, unsigned int capacity)
 {
     assert(heap == nullptr);
@@ -62,7 +57,6 @@ int PQheap<T,C,S>::bind(T* a, unsigned int capacity)
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 int PQheap<T,C,S>::unbind()
 {
     heap = nullptr;
@@ -71,7 +65,6 @@ int PQheap<T,C,S>::unbind()
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 PQheap<T,C,S>::~PQheap()
 {
     // be sure to run unbind() first if using external buffer for heap
@@ -81,7 +74,6 @@ PQheap<T,C,S>::~PQheap()
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 int PQheap<T,C,S>::insert(const T& t)
 {
     if (cap == 0)
@@ -105,7 +97,6 @@ int PQheap<T,C,S>::insert(const T& t)
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 int PQheap<T,C,S>::heapUp()
 {
     return heapUp(N);
@@ -124,7 +115,6 @@ int PQheap<T,C,S>::heapUp()
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 int PQheap<T,C,S>::heapUp(int i)
 {
     assert(N > 1);
@@ -147,7 +137,6 @@ int PQheap<T,C,S>::heapUp(int i)
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 int PQheap<T,C,S>::extractTop(T& t)
 {
     if (N < 1) return 1;
@@ -167,14 +156,12 @@ int PQheap<T,C,S>::extractTop(T& t)
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 T PQheap<T,C,S>::peakTop() const
 {
     return N > 0 ? heap[S] : T();
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 int PQheap<T,C,S>::peakTop(const T* t) const
 {
     if (N < 1) return 1;
@@ -185,7 +172,6 @@ int PQheap<T,C,S>::peakTop(const T* t) const
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 void PQheap<T,C,S>::discardTop()
 {
     if (N < 1) return;
@@ -198,7 +184,6 @@ void PQheap<T,C,S>::discardTop()
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 T PQheap<T,C,S>::extractTop()
 {
     T top;
@@ -207,7 +192,6 @@ T PQheap<T,C,S>::extractTop()
 }
 
 //template <typename T>
-//CUDA_CALLABLE
 //int PQheap<T>::heapDown()
 //{
 //    //return heapDown(1);
@@ -235,7 +219,6 @@ T PQheap<T,C,S>::extractTop()
 //}
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 int PQheap<T,C,S>::heapDown()
 {
     //return heapDown(1);
@@ -265,7 +248,6 @@ int PQheap<T,C,S>::heapDown()
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 int PQheap<T,C,S>::heapDownQuick()
 {
     //return heapDown(1);
@@ -295,7 +277,6 @@ int PQheap<T,C,S>::heapDownQuick()
 
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 int PQheap<T,C,S>::heapDown(int i)
 {
     assert(N > 1);
@@ -321,7 +302,6 @@ int PQheap<T,C,S>::heapDown(int i)
 }
 
 template <typename T, typename C, int S>
-CUDA_CALLABLE
 int PQheap<T,C,S>::heapify()
 {
     return heapDown();
@@ -329,7 +309,6 @@ int PQheap<T,C,S>::heapify()
 
 #if _MSC_VER < 1800
 template <typename T, typename C, int S>
-CUDA_CALLABLE CUDA_INLINE
 void PQheap<T,C,S>::swap(T& a, T& b) {
     T tmp = a;
     a = b;
@@ -341,7 +320,6 @@ void PQheap<T,C,S>::swap(T& a, T& b) {
 #define STR(x) #x
 #pragma message("C++: " XSTR(__cplusplus) ", MSVC: " XSTR(_MSC_VER))
 template <typename T, typename C, int S>
-CUDA_CALLABLE CUDA_INLINE
 void PQheap<T,C,S>::swap(T& a, T& b) {
     T tmp(std::move(a));
     a = std::move(b);
